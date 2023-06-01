7db811b9e85426724fbfcad748e21c9104b8e08d
Vincent Guarnieri <126090788+VinGuar@users.noreply.github.com>
2023-06-01 21:01:12 +0000

    a

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,3 +1,4 @@
+'use client';
 import Image from 'next/image'
 import styles from '../app/page.module.css'
 import '../app/globals.css';
@@ -7,17 +8,29 @@ import Footer from '../comps/Footer'
 
 
 export default function Home() {
+
   return (
     <main>
       <Navbar />
       <div className={styles.whole}>
+
         <div className={styles.body1}>
           <p className = {styles.container}>Hello World! I am Vincent Guarnieri.</p>
           <img src="waving4.png" width="400px" height="400px" style={{"margin-top":"0px"}} />
         </div>
-        <div className={styles.body2}>
+
+        <div id = "body2" className={styles.body2}>
           <p>second</p>
         </div>
+
+        <div id = "body3" className={styles.body2}>
+          <p>3rf</p>
+        </div>
+
+        <div id = "body4" className={styles.body2}>
+          <p>4th</p>
+        </div>
+
       </div>
       <Footer />
 
--- my-app/comps/Navbar.js
+++ my-app/comps/Navbar.js
@@ -1,33 +1,43 @@
+"use client";
+import { Passero_One } from "next/font/google";
 import styles from "../comps/Navbar.module.css"
 import Link from 'next/link';
 
 const Navbar = () => {
 
+    const handleClick = (num) => {
+        console.log('Clicked!');
+        if (typeof window !== 'undefined') {
+          if (num === 2){
+            document.getElementById('body2').scrollIntoView({ behavior: 'smooth' });
+          }
+        }
+      };
+
     return ( 
-        <nav>
-            
+            <nav>
+
             <main className={styles.whole}>
-                {/* 
-                <div className={styles.left}>
-                    <img src="paper.png" alt="Folded Paper icon" width="70" height="60"/>
-                </div> 
-                 */}
                 <div className={styles.menu}>
+
                     <span className={styles.paren}>&#9790;</span>
+
                     <Link href="" className = {styles.items}>Home</Link>
-                    <Link href = "/about" className = {styles.items}>Projects</Link>
-                    <Link href = "/about" className = {styles.items}>About</Link>
-                    <Link href = "/about" className = {styles.items}>Resume</Link>
+                    <div style={{'cursor':'pointer'}} onClick = {() => handleClick(2)} className = {styles.items}>2</div>
+                    <div style={{'cursor':'pointer'}} onClick = {() => handleClick(3)} className = {styles.items}>3</div>
+                    <div style={{'cursor':'pointer'}} onClick = {() => handleClick(4)} className = {styles.items}>4</div>
 
                     <span className={styles.paren}>&#9789;</span>
 
                 </div>
             </main>
-           
-        </nav>
-     );
-}
+            </nav>
 
+     );
+
+
+     
+}
 
 
  
