716da99670391f969484f4e1022e89b9d6569604
Vincent Guarnieri <126090788+VinGuar@users.noreply.github.com>
2023-06-07 21:08:38 +0000

    fas

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -14,47 +14,57 @@ export default function Home() {
       <Navbar />
       <div className={styles.whole}>
 
-        <div id = "Start" className={styles.top}>
+        <section id = "Start" className={styles.top}>
           <p className = {styles.container}>Hello World! I am Vincent Guarnieri.</p>
           <img src="waving4.png" width="400px" height="400px" style={{"margin-top":"0px"}} />
-        </div>
+        </section>
 
-        <div id = "About" className={styles.about}>
+        <section id = "About" className={styles.about}>
 
           <div className={styles.aboutmain}>
             <p className={styles.holder}>About Me!</p>
             <p className={styles.holder}>Email: vincentguarnieri1@gmail.com</p>
-            <p className={styles.holder}>LinkedIn: Click Here!</p>
+            <p className={styles.holder}>Résumé: make button</p>
+            <p className={styles.holder}>Github: make button</p>
+            <p className={styles.holder}>LinkedIn: make button</p>
+
           </div>
 
           <div className={styles.aboutsections}>
-            <div className={styles.aboutind}>          
-              <p className={styles.aboutpara}>Professional Interests</p>
+            <div className={styles.aboutind} style ={{"background-color": "rgb(250, 228, 255)"}}>    
+              <img src="interest.png" className={styles.aboutimgs} />      
+              <h1 className={styles.aboutheaders}>Professional Interests</h1>
             </div>
-            <div className={styles.aboutind}>          
-              <p className={styles.aboutpara}>Hobbies and Passions</p>
+            <div className={styles.aboutind} style ={{"background-color": "rgb(255, 239, 228)"}}>          
+              <img src="hobbies.png" className={styles.aboutimgs} />
+              <h1 className={styles.aboutheaders}>Hobbies and Passions</h1>
             </div>
-            <div className={styles.aboutind}>          
-              <p className={styles.aboutpara}>Skills</p>
+            <div className={styles.aboutind} style ={{"background-color": "rgb(241, 255, 228)"}}>          
+              <img src="experience.png" className={styles.aboutimgs} />
+              <h1 className={styles.aboutheaders} >Skills</h1>
             </div>          
-            <div className={styles.aboutind}>          
-              <p className={styles.aboutpara}>Values</p>
+            <div className={styles.aboutind} style ={{"background-color": "rgb(255, 253, 228)"}}>          
+              <img src="balance.png" className={styles.aboutimgs} />
+              <h1 className={styles.aboutheaders}>Values</h1>
             </div>
           </div>
 
-        </div>
+        </section>
 
-        <div id = "Projects" className={styles.projects}>
-          <p >3rf</p>
-        </div>
+        <section id = "Projects" className={styles.projects}>
+          <p className={styles.projectspara}>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+          Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.          Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+          Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+          </p>
+        </section>
 
-        <div id = "Experience" className={styles.experience}>
-          <p>4th</p>
-        </div>
+        <section id = "Experience" className={styles.experience}>
+          <p className={styles.experiencepara}>4th</p>
+        </section>
 
-        <div id = "Education" className={styles.education}>
-          <p>5th</p>
-        </div>
+        <section id = "Education" className={styles.education}>
+          <p className={styles.educationpara}>5th</p>
+        </section>
 
 
       </div>
--- my-app/comps/Navbar.js
+++ my-app/comps/Navbar.js
@@ -2,13 +2,80 @@
 import { Passero_One } from "next/font/google";
 import styles from "../comps/Navbar.module.css"
 import Link from 'next/link';
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
+
+
 
 const Navbar = () => {
+
+    const [clicked1, setClicked1] = useState(true);
+    const [clicked2, setClicked2] = useState(true);
+    const [clicked3, setClicked3] = useState(true);
+    const [clicked4, setClicked4] = useState(true);
+    const [clicked5, setClicked5] = useState(true);
+
+    useEffect(() => {
+      const scroll = () => {
+        const sections = document.querySelectorAll("section");
+
+        sections.forEach((section) => {
+          const top = window.scrollY;
+          const offset = section.offsetTop - 300;
+          const height = section.offsetHeight;
+          const id = section.getAttribute("id");
+          var currid = "Start";
+
+
+          if (top >= offset && top < offset + height) {
+
+            if (id==="Start"){
+              setClicked1(current => false)
+              currid = id;
+
+            } else if (id==="About"){
+              setClicked2(current => false)
+              currid = id;
+
+            } else if (id==="Projects"){
+              setClicked3(current => false)
+              currid = id;
+            } else if (id==="Experience"){
+              setClicked4(current => false)
+              currid = id;
+            } else if (id==="Education"){
+              setClicked5(current => false)
+              currid = id;
+            }
+          } else {
+
+            if (id==="Start"){
+              setClicked1(current => true)
+            } else if (id==="About"){
+              setClicked2(current => true)
+            } else if (id==="Projects"){
+              setClicked3(current => true)
+            } else if (id==="Experience"){
+              setClicked4(current => true)
+            } else if (id==="Education"){
+              setClicked5(current => true)
+            }
+
+          }
+        });
+      };
+  
+      window.addEventListener("scroll", scroll);
+  
+      return () => {
+        window.removeEventListener("scroll", scroll);
+      };
+    }, []);
+   
 
     const handleClick = (num) => {
       
         console.log('Clicked!');
-        if (typeof window !== 'undefined') {
           if (num === 1){
             document.getElementById('Start').scrollIntoView({ behavior: 'smooth'});
           } else if (num === 2){
@@ -24,7 +91,7 @@ const Navbar = () => {
             document.getElementById('Education').scrollIntoView({ behavior: 'smooth' });
         }
 
-        }
+        
 
       };
 
@@ -34,15 +101,15 @@ const Navbar = () => {
               <main id="Main" className={styles.whole}>
                   <div className={styles.menu}>
 
-                      <span className={styles.paren}>&#9790;</span>
+                      <span className={styles.parenleft}>&#9790;</span>
 
-                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(1)} className = {styles.items}>Home</div>
-                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(2)} className = {styles.items}>About!</div>
-                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(3)} className = {styles.items}>Projects</div>
-                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(4)} className = {styles.items}>Experience</div>
-                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(5)} className = {styles.items}>Education</div>
+                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(1)}className = {clicked1 ? styles.display : styles.hide}>Home</div>
+                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(2)} className = {clicked2 ? styles.display : styles.hide}>About!</div>
+                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(3)} className = {clicked3 ? styles.display : styles.hide}>Projects</div>
+                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(4)} className = {clicked4 ? styles.display : styles.hide}>Experience</div>
+                      <div style={{'cursor':'pointer'}} onClick = {() => handleClick(5)} className = {clicked5 ? styles.display : styles.hide}>Education</div>
 
-                      <span className={styles.paren}>&#9789;</span>
+                      <span className={styles.parenright}>&#9789;</span>
 
                   </div>
               </main>
